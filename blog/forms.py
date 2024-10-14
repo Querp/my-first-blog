33f1d056f133ced8518e271bf4be77487d26174e
Your Name <you@example.com>
2024-10-14 18:00:47 +0200

    Add input range smaak and malsheid.

--- blog/forms.py
+++ blog/forms.py
@@ -6,5 +6,10 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        # fields = ('author', 'title', 'text', 'created_date', 'published_date')
+        # fields = ('author', 'title', 'text', 'created_date', 'published_date', 'smaak')
         fields = '__all__'
+        widgets = {
+            'smaak': forms.NumberInput(attrs={'type': 'range', 'min': '0', 'max': '100', 'oninput':'this.nextElementSibling.value = this.value' }),
+            'malsheid': forms.NumberInput(attrs={'type': 'range', 'min': '0', 'max': '100', 'oninput':'this.nextElementSibling.value = this.value' }),
+        } 
+   
